7b0e21e9657ac988d01087319ccc200799d2006c
mmbits <malinm@uw.edu>
2019-07-08 10:40:55 -0700

    office hour: added NamePicker

--- src/App.js
+++ src/App.js
@@ -4,30 +4,65 @@ import logo from './swirly.png';
 //  "Use these free PNG Logo Design for your personal projects or designs."
 import './App.css';
 import TextInput from './TextInput'
+import NamePicker from './NamePicker'
 
 class App extends React.Component {
   state={
-    messages:[]
+    messages:[],
+    name:'',
+    editName:false,
   }
 
-  sendMessage = (m) => {
+  componentWillMount(){
+    var name = localStorage.getItem('name')
+    if(name) {
+      this.setState({name})
+    }
+  }
+
+  sendMessage = (text) => {
+    var m = {
+      text,
+      name: this.state.name,
+    }
     var messages = [...this.state.messages, m]
     this.setState({messages})
   }
 
+  setEditName = (editName) => {
+    if(!editName){
+      localStorage.setItem('name', this.state.name)
+    }
+    this.setState({editName})
+  }
+
   render() {
-    var {messages} = this.state
+    var {messages, name, editName} = this.state
     return (
       <div className="App">
         <header className="headerer">
-          <img src={logo} alt="stolen logo" className="logo" />
+          <img
+            src={logo}
+            alt="stolen logo"
+            className="logo"
+          />
           Chatter
+          <NamePicker
+            name = {name}
+            editName={editName}
+            // changeName={function(value){ this.setState({name: value}).bind(this)} }
+            changeName={value=> this.setState({name: value})}
+            setEditName={this.setEditName}
+          />
         </header>
         <main className="messages">
           {messages.map((m,i)=>{
-            return (<div key={i}>
-              {m}
-              </div>)
+            return (<div key={i} className="bubble-wrap" from={m.name===name ? "me" : "you"}>
+              {m.name!==name && <div className="bubble-name">{m.name}</div>}
+              <div className="bubble">
+                <span>{m.text}</span>
+              </div>
+            </div>)
           })}
         </main>
         <TextInput sendMessage={this.sendMessage} />
